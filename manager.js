b20cb40cee6e56cba5ee1bd90d299c4c9273385d
Mytt2024 <mytt@l00th.com>
2024-09-03 12:54:27 +0500

    Update manager.js

--- manager.js
+++ manager.js
@@ -9,7 +9,7 @@ let scriptOutput = [];
 
 // Function to run the app.js script
 function runScript() {
-    const url = "https://dhenme.com";
+    const url = "https://raajje.mv";
     const port = "12000";
     const param1 = "1";
     const param2 = "1";
